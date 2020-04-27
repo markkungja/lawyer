aad8f2f09314a1f03741d810e1c1c6804e4dbc15
markkungja <mark_kungja@hotmail.com>
2020-04-27 09:19:21 +0700

    test update

--- session.php
+++ session.php
@@ -3,6 +3,7 @@
 	ob_start();
 	echo "<h3> PHP List All Session Variables</h3>";
     foreach ($_SESSION as $key=>$val)
-    echo $key." ".$val."<br/>";
+	echo $key." ".$val."<br/>";
+	//test update
 	
 ?>
